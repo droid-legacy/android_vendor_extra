f83ceecc2d1067921ec5e835a6d590ca06380c89
SirRGB <sirrgb@proton.me>
2026-02-07 16:04:18 +0100

    feat: import prebuilt fonts/overlays from crdroid
    * https://github.com/crdroidandroid/android_vendor_addons/tree/2968e820a86065a63c05a04ee79535df0863fbca/prebuilt/product
    * https://github.com/crdroidandroid/android_vendor_addons/tree/2968e820a86065a63c05a04ee79535df0863fbca/themes/fonts
    * sirrgb: remove lineage dups
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- product.mk
+++ product.mk
@@ -1,3 +1,41 @@
 PRODUCT_PACKAGES += \
-    custom_fonts_customization.xml
+    custom_fonts_customization.xml \
+    FontAccuratistOverlay \
+    FontAclonicaOverlay \
+    FontAmaranteOverlay \
+    FontBariolOverlay \
+    FontCagliostroOverlay \
+    FontCoconOverlay \
+    FontComfortaaOverlay \
+    FontComicSansOverlay \
+    FontCookieRunOverlay \
+    FontCoolstoryOverlay \
+    FontExotwoOverlay \
+    FontFifa2018Overlay \
+    FontGrandHotelOverlay \
+    FontHarmonySansOverlay \
+    FontIBMPlexSansOverlay \
+    FontLinotteOverlay \
+    FontNokiaPureOverlay \
+    FontNothingDotHeadlineOverlay \
+    FontNothingDotOverlay \
+    FontNunitoOverlay \
+    FontOneplusSansOverlay \
+    FontOneplusSlateOverlay \
+    FontOswaldOverlay \
+    FontPlayOverlay \
+    FontQuandoOverlay \
+    FontRedressedOverlay \
+    FontReemKufiOverlay \
+    FontRobotoCondensedOverlay \
+    FontRookeryOverlay \
+    FontSanFranciscoDisplayProSourceOverlay \
+    FontSamsungOneOverlay \
+    FontSansSerifOverlay \
+    FontSonySketchOverlay \
+    FontStoropiaOverlay \
+    FontSurferOverlay \
+    FontUbuntuOverlay
 
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/extra/fonts/prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts)
